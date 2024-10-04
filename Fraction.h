ae100c2a8d313179f80ffeeb08c38f0469b2a3b3
CharlieNielson <charlienielson@mail.weber.edu>
2024-10-04 12:16:19 -0600

    Update Fraction.h

--- Fraction.h
+++ Fraction.h
@@ -10,12 +10,17 @@ class Fraction
        Fraction(int, int); //Overloaded Constructor differs in number or type of parameters
        Fraction(const Fraction & other); //Copy Constructor: parameter is the class
        const Fraction operator + (Fraction other); //Overloaded operator
+       const Fraction operator - (Fraction other); //Overloaded operator
+       const Fraction operator / (Fraction other); //Overloaded operator
+       const Fraction operator * (Fraction other); //Overloaded operator
        int gcd(int, int);  //Method
        friend ostream& operator << (ostream& os, const Fraction &f);//Friend function has access to private variables
        int getNumerator(); //getter method
+       int getDenominator();
    private:
        int numerator, denominator;
    
 
 
 };
+
