0accca015adbb910ad7675384bbbed1c716cf741
CharlieNielson <charlienielson@mail.weber.edu>
2024-10-04 09:01:20 -0600

    Update No Brain.cpp

--- No Brain.cpp
+++ No Brain.cpp
@@ -8,8 +8,14 @@ using namespace std;
 int main() {
 
     cout << "Testing time.\n";
-    //Time t1;
-    Time t2(1, 3, 5);
+    Time t1(0, 59, 53);
+    Time t2(1, 60, 5);// 01:03:05
 
+    Time t3 = t1 + t2;
+    cout << t3;
+    cout << "Add 10 seconds: ";
+    t3 += 10;
+    cout << t3 << endl;
+    
     return 0;
 }
