b3e8b2921077c754e627b89e3df7355ccc200b82
CharlieNielson <charlienielson@mail.weber.edu>
2024-10-04 08:40:32 -0600

    Update No Brain.cpp

--- No Brain.cpp
+++ No Brain.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
-using namepace std;
+#include "time.h"
+using namespace std;
+
+
+
+
+int main() {
+
+    cout << "Testing time.\n";
+    //Time t1;
+    Time t2(1, 3, 5);
+
+    return 0;
+}
