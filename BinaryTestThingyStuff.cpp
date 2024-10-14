31b815670e927b4532665722d7c4b9db5708fdf9
CharlieNielson <charlienielson@mail.weber.edu>
2024-10-13 20:42:17 -0600

    Update BinaryTestThingyStuff.cpp

--- BinaryTestThingyStuff.cpp
+++ BinaryTestThingyStuff.cpp
@@ -1,11 +1,46 @@
 #include <iostream>
 #include <sstream>
-#include "BinaryNumber.h"
+#include "BinaryOperations.h"
 
 using namespace std;
 
-int main() {
+operations::operations() {
+    nibble = 0;
+    cout << "default\n";
+}
 
+operations::operations(string input) {
+    int end = 0;
+    // WIP
+    nibble = end;
+}
+
+operations::operations(int imp) {
+    int end = 0;
+    int copy = imp;
+    while (copy > 0) {
+        end += copy % 10;
+        copy /= 10;
+    }
+    nibble = end;
+}
+
+int operations::addition(int thing, int other) {
+    int end = thing + other;
+    return end;
+}
+
+int operations::subtraction(int thing, int other) {
+    int end = thing - other;
+    return end;
+}
+
+
+int main() {
+    operations b1 = 101;
+    operations b2 = 110;
+    int num = 101, num2 = 110;
+    operations addition(num, num2);
 
     return 0;
 }
